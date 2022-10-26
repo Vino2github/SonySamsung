22783657a59de601687adc223992d10b83f8afcd
vino2github <vinothini.devi.k.l@gmail.com>
2022-10-26 14:00:21 +0530

    committed chnages sonysamsung

---
+++ SonySamsung/src/main/java/sony/SamsumgClass.java
@@ -0,0 +1,10 @@
+package sony;
+
+public class SamsumgClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Samsung class results");
+	}
+
+}
---
+++ SonySamsung/src/main/java/sony/SonyClass.java
@@ -0,0 +1,8 @@
+package sony;
+
+public class SonyClass {
+
+	public static void main(String[] args) {
+		System.out.println("Sony class results");
+	}
+}
