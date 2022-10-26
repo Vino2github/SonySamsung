afd04a4716192aeb943f919ba8e4e03b8b500c7f
Vino2github <113656488+Vino2github@users.noreply.github.com>
2022-10-26 16:08:37 +0530

    Update SamsumgClass.java

--- SonySamsung/src/main/java/sony/SamsumgClass.java
+++ SonySamsung/src/main/java/sony/SamsumgClass.java
@@ -5,6 +5,8 @@ public class SamsumgClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Samsung class results");
+		System.out.println("GITHUB Samsung results");
+		
 	}
 
 }
