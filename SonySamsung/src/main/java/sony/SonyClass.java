07236234b5b748a9622b33c1e48059600146e3d7
vino2github <vinothini.devi.k.l@gmail.com>
2022-10-26 16:16:02 +0530

    pushed changes

--- SonySamsung/src/main/java/sony/SonyClass.java
+++ SonySamsung/src/main/java/sony/SonyClass.java
@@ -2,7 +2,19 @@ package sony;
 
 public class SonyClass {
 
+	static int a ;
 	public static void main(String[] args) {
+		
 		System.out.println("Sony class results");
+		method1(20);
+		System.out.println("Sony class results"+ a);
+	}
+	
+	public static void method1(int a)
+	{
+		a=40;
+		System.out.println("Hello world: "+ a);
+		
+		
 	}
 }
